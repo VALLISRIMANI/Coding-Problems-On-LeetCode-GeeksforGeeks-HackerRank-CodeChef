de816573d1ed922391f5ed798b9adec69f95b638
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-12 21:55:56 +0530

    ✅ MUL123 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/MUL123/solution.java
+++ codechef/medium/MUL123/solution.java
@@ -7,6 +7,25 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
 
+        while (T-- > 0) {
+            int N = sc.nextInt();
+
+            if (N % 3 == 0) {
+                System.out.println(0);
+            } else if (N % 5 == 0) {
+                System.out.println(2);
+            } else {
+                if (N % 3 == 2) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(1);
+                }
+            }
+        }
+
+        sc.close();
 	}
 }
