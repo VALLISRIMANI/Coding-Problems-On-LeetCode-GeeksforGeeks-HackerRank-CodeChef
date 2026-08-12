1464a44071017f7beb0c5ad110dfc1720b86bd01
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-12 15:14:27 +0530

    ✅ Stock Buy and Sell – Max 2 Transactions Allowed | Hard | Java | Runtime: N/A | Memory: N/A | 2026-08-12

--- gfg/hard/buy-and-sell-a-share-at-most-twice/solution.java
+++ gfg/hard/buy-and-sell-a-share-at-most-twice/solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public static int maxProfit(int[] prices) {
         // code here
-        int n = prices.length;
+        /* int n = prices.length;
         if (n == 0) return 0;
 
         int[] left = new int[n];
@@ -28,5 +28,22 @@ class Solution {
         }
 
         return maxProfit;
+        */
+        
+        int buy1 = Integer.MIN_VALUE;
+        int sell1 = 0;
+        
+        int buy2 = Integer.MIN_VALUE;
+        int sell2 = 0; 
+        
+        for (int price : prices) {
+            buy1 = Math.max(buy1, -price);
+            sell1 = Math.max(sell1, buy1 + price);
+            
+            buy2 = Math.max(buy2, sell1 - price);
+            sell2 = Math.max(sell2, buy2 + price);
+        }
+        
+        return sell2;
     }
 }
