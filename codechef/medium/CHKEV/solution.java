2d00479e0d33abfdf8c3e9228596db40ef96f2ab
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-12 21:52:10 +0530

    ✅ CHKEV | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/CHKEV/solution.java
+++ codechef/medium/CHKEV/solution.java
@@ -7,6 +7,17 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+        Scanner sc = new Scanner(System.in);
 
+        int L = sc.nextInt();
+        int R = sc.nextInt();
+
+        if (L % 2 == 0 || R % 2 == 0 || L < R) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        
+        sc.close();
 	}
 }
