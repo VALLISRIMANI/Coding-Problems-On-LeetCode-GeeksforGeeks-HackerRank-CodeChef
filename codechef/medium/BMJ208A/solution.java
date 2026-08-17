487c6b1367efde51f092fae3d1aea373bac79377
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-17 16:09:48 +0530

    ✅ BMJ208A | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/BMJ208A/solution.java
@@ -0,0 +1,27 @@
+// Click on 'Submit' to check the output of the functions
+// Click on 'Next' to proceed
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = 45;
+		int b = 36;
+    	
+    	// Output the minimum value of 'a' and 'b'
+		System.out.println(Math.min(a, b));
+		
+		// Output the maximum value of 'a' and 'b'
+		System.out.println(Math.max(a, b));
+		
+		// Output the absolute value of the difference of 'b' and 'a'
+		System.out.println(Math.abs(b - a));
+		
+		int c = 6;
+        int d = 8;
+        
+        // Output the remainder when 'b' is divided by 'c' and 'd'
+        System.out.println(b % c);
+        System.out.println(b % d);
+	}
+}
