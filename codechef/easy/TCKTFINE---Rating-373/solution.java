50c2cf6514ccf7a9c98f40fc3446d74c9d4ada17
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-18 14:52:45 +0530

    ✅ TCKTFINE - Rating 373 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/easy/TCKTFINE---Rating-373/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner scanner = new Scanner(System.in);
+        
+        // Read the number of test cases
+        if (scanner.hasNextInt()) {
+            int t = scanner.nextInt();
+            
+            while (t-- > 0) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                int c = scanner.nextInt();
+                
+                System.out.println(a * (b - c));
+            }
+        }
+        
+        scanner.close();
+	}
+}
