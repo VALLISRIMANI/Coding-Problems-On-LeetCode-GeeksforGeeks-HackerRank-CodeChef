12181fe55bc26128fcabce8ac0f75d31d96f68d4
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-19 16:04:41 +0530

    ✅ JAPROJPR04 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/JAPROJPR04/solution.cpp
@@ -0,0 +1,20 @@
+    }
+            return "Hot";
+        }
+            return "Cold";
+        } else if (number == guess) {
+            return "Right";
+        } else {
+    public static String giveHint(int number, int guess) {
+        if (guess > (number + 10) || guess < (number - 10)) {
+    // Method to provide hints based on the user's guess
+
+    }
+        return rand.nextInt(100) + 1;
+        Random rand = new Random();
+    public static int getRandomNumber() {
+    // Method to generate a random number between 1 and 100
+
+import java.util.*;
+
+public class Codechef {
