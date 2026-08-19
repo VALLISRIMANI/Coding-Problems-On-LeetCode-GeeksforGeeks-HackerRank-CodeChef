e5e0aedce0f898f9a6fe1b8d512c8f08bfc45d01
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-19 16:11:22 +0530

    ✅ JAPROJPR07 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/JAPROJPR07/solution.cpp
@@ -0,0 +1,20 @@
+    public static String calculatorDisplay() {
+        String display = "Welcome to Calculator\n\nChoose one operation:\n1. Add\n2. Subtract\n3. Exit\n";
+
+
+
+
+        
+        return display;
+    }
+
+    public static void main(String[] args) {
+        // Displaying the output from the calculatorDisplay method
+        System.out.print(calculatorDisplay());
+    }
+}
+
+
+public class Codechef {
+
+import java.util.*;
