c8d35b6544e02df9e4d7e2c72838f5c6dd5be040
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-21 14:04:01 +0530

    ✅ MATNEGCOUNT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/MATNEGCOUNT/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int[][] mat = new int[n][m];
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                mat[i][j] = scanner.nextInt();
+                if (mat[i][j] < 0) count++;
+            }
+        }
+
+        System.out.println(count);
+	}
+}
