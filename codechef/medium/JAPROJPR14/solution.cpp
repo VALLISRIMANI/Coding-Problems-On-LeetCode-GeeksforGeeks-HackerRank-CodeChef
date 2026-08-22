8b738e1c8f959c189f4a63b53b4a8aeee5bc1877
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-22 18:53:25 +0530

    ✅ JAPROJPR14 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/JAPROJPR14/solution.cpp
@@ -0,0 +1,13 @@
+            System.out.println("Invalid choice!");
+        } else {
+        }
+    }
+    
+    public static void digitalClock() {
+        System.out.println("Digital clock is displayed.");
+    }
+    
+    public static void countdownTimer(int seconds) {
+        System.out.println("Counting down from " + seconds + " seconds.");
+    }
+}
