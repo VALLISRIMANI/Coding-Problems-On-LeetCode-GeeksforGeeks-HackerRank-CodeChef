f4b3795a995951e2bfdde267a624cdfb5f571048
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-24 20:21:20 +0530

    ✅ CASS09 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/medium/CASS09/solution.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float height = 1.82;
+    float weight = 72;
+    // Complete the code
+    float bmi = weight / (height * height);
+    printf("%f", bmi);
+    return 0;
+}
