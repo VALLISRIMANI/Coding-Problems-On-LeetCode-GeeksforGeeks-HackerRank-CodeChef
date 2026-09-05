94d5cfcd4ff2a9bc359f5a5bd842c58637c15a5a
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-09-05 12:34:42 +0530

    ✅ MATROTATE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/medium/MATROTATE/solution.java
@@ -0,0 +1,18 @@
+public static void rotateClockwise(int[][] matrix) {
+    // Reverse the matrix
+    int n = matrix.length;
+    for (int i = 0; i < n / 2; i++) {
+        int[] temp = matrix[i];
+        matrix[i] =  matrix[n - i - 1];
+        matrix[n - i - 1] = temp;
+    }
+
+    // Transpose the matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
