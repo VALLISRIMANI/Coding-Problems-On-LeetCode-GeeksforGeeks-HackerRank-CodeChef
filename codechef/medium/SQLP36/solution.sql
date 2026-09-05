8c5cdce7a5fb946ec349aa1b6372c85540b76656
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-09-05 12:33:06 +0530

    ✅ SQLP36 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-05

--- codechef/medium/SQLP36/solution.sql
+++ codechef/medium/SQLP36/solution.sql
@@ -1,6 +1,10 @@
--- your code goes here
-select user_id, count(property_id) as favorite_count 
-from favorites 
-group by user_id
-order by favorite_count desc
-limit 1;
+SELECT prop.property_id, prop.title, prop.location
+FROM Properties prop
+WHERE prop.property_id IN (
+    SELECT pa.property_id
+    FROM PropertyAmenities pa
+    JOIN Amenities am ON pa.amenity_id = am.amenity_id
+    WHERE am.name IN ('Heating', 'Kitchen')
+    GROUP BY pa.property_id
+    HAVING COUNT(DISTINCT am.name) = 2
+);
