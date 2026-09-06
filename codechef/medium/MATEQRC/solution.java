9616414f0dba9f340b9c044d4df40fbe308bc7e4
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-09-06 16:28:51 +0530

    ✅ MATEQRC | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/medium/MATEQRC/solution.java
+++ codechef/medium/MATEQRC/solution.java
@@ -1,45 +1,49 @@
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static int equalPairs(int[][] mat) {
-        int ans = 0;
-        HashMap<String, Integer> mp = new HashMap<>();
+    public static int firstOne(int[] row, int low, int high) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if ((mid == 0 || row[mid - 1] == 0) && row[mid] == 1) {
+                return mid;
+            } else if (row[mid] == 0) {
+                return firstOne(row, mid + 1, high);
+            } else {
+                return firstOne(row, low, mid - 1);
+            }
+        }
+        return -1; 
+    }
 
+    public static int maxOneRow(int[][] mat) {
+        int maxones = 0;
+        int rowIdx = 0;
         for (int i = 0; i < mat.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < mat[i].length; j++) {
-                sb.append(mat[i][j]);
-                sb.append(",");
+            int ones = mat[i].length - firstOne(mat[i], 0, mat[i].length - 1);
+            if (ones > maxones) {
+                maxones = ones;
+                rowIdx = i;
             }
-            mp.put(sb.toString(), mp.getOrDefault(sb.toString(), 0) + 1);
         }
-
-        for (int i = 0; i < mat[0].length; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < mat.length; j++) {
-                sb.append(mat[j][i]);
-                sb.append(",");
-            }
-            ans += mp.getOrDefault(sb.toString(), 0);
-        }
-
-        return ans;
+        return rowIdx + 1;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
+        int m = scanner.nextInt();
         assert (1 <= n && n <= 100);
+        assert (1 <= m && m <= 100);
 
-        int[][] mat = new int[n][n];
+        int[][] mat = new int[n][m];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 mat[i][j] = scanner.nextInt();
+                assert (mat[i][j] == 0 || mat[i][j] == 1);
             }
         }
 
-        System.out.println(equalPairs(mat));
+        System.out.println(maxOneRow(mat));
     }
 }
