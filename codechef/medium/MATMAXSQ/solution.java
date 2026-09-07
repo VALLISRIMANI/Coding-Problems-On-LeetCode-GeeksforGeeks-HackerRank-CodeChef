21e6909525d89c678b31011ea6d2d3460c54d5f9
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-09-07 21:41:32 +0530

    ✅ MATMAXSQ | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/MATMAXSQ/solution.java
+++ codechef/medium/MATMAXSQ/solution.java
@@ -1,47 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int minPathSum(int[][] mat) {
+    public static int maximalSquare(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         
+        int maxside = 0;
         int[][] dp = new int[m][n];
         
-        dp[0][0] = mat[0][0];
-        
-        for(int i = 1; i < m; i++) {
-            dp[i][0] = dp[i - 1][0] + mat[i][0];
-        }
-        
-        for(int j = 1; j < n; j++) {
-            dp[0][j] = dp[0][j - 1] + mat[0][j];
-        }
-        
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + mat[i][j];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || mat[i][j] == 0) {
+                    dp[i][j] = mat[i][j];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+                
+                maxside = Math.max(maxside, dp[i][j]);
             }
         }
         
-        return dp[m - 1][n - 1];
+        return maxside * maxside;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-        int m = scanner.nextInt();
-
+        
         assert (1 <= n && n <= 100);
-        assert (1 <= m && m <= 100);
 
-        int[][] mat = new int[n][m];
+        int[][] mat = new int[n][n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < n; j++) {
                 mat[i][j] = scanner.nextInt();
+                assert (mat[i][j] == 0 || mat[i][j] == 1);
             }
         }
 
-        System.out.println(minPathSum(mat));
+        System.out.println(maximalSquare(mat));
     }
 }
