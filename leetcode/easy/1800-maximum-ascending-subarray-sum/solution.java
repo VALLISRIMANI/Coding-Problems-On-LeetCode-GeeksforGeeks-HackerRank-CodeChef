c9bb158463d79dd8dc3a3f055b3eaf0770e8580a
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-09-11 09:35:39 +0530

    ✅ Maximum Ascending Subarray Sum | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.3 MB | 2026-09-11

--- leetcode/easy/1800-maximum-ascending-subarray-sum/solution.java
+++ leetcode/easy/1800-maximum-ascending-subarray-sum/solution.java
@@ -4,12 +4,12 @@ class Solution {
         int maxSum = Integer.MIN_VALUE;
 
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < nums[i - 1]) {
+            if (nums[i] <= nums[i - 1]) {
                 maxSum = Math.max(maxSum, sum);
                 sum = 0;
-            } else {
-                sum += nums[i];
             }
+            
+            sum += nums[i];
         }
         
         maxSum = Math.max(maxSum, sum);
